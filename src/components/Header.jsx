2b7e6cc092992f4121d9a6b1a339d5ef24c21288
maksebdevNessrine <maksebdev3@gmail.com>
2024-11-20 16:52:40 +0100

    fix header and profile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,15 +29,15 @@ function Header() {
 
     return (
         <header className="bg-sky-950 text-white sticky top-0 z-50">
-            <div className="flex justify-between items-center px-4 py-2 flex-wrap">
-                <div className="flex flex-col items-start">
+            <div className="w-full max-w-screen-xl mx-auto px-4 py-2 flex justify-between items-center">
+                <div className="flex flex-col md:flex-row md:items-center md:space-x-4">
                     <div
                         className="w-[60px] h-[60px] bg-cover"
                         style={{ backgroundImage: `url(${logoPath})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}
                     ></div>
-                    <h1 className="text-xl font-bold text-left text-[#d6b884]">lavocato</h1>
+                    <h1 className="text-xl font-bold text-[#d6b884]  md:mt-0">lavocato</h1>
                 </div>
-                <div className="flex items-center ml-auto mt-2">
+                <div className="relative">
                     {!isConnected ? (
                         <>
                             <GoogleLogin
@@ -62,11 +62,11 @@ function Header() {
                             <button 
                                 id="dropdownAvatarNameButton" 
                                 onClick={toggleDropdown} 
-                                className="flex items-center text-sm font-medium text-[#d6b884] rounded-full hover:text-white-100" 
+                                className="flex justify-end items-center text-m font-medium text-[#d6b884] rounded-full hover:text-white-100" 
                                 type="button"
                             >
                                 <span className="sr-only">Open user menu</span>
-                                <img className="w-8 h-8 me-2 rounded-full" src="https://toplawyerscanada.ca/wp-content/uploads/Criminal-Defence-Lawyer-Toronto-Richard-Posner.jpg" alt="user photo" />
+                                <img className="w-10 h-10 me-2 rounded-full" src="https://toplawyerscanada.ca/wp-content/uploads/Criminal-Defence-Lawyer-Toronto-Richard-Posner.jpg" alt="user photo" />
                                 Bonnie Green
                                 <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
@@ -74,7 +74,7 @@ function Header() {
                             </button>
                             {/* Dropdown menu */}
                             {dropdownOpen && (
-                                <div className="absolute right-0 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+                                <div className="absolute right-0 mt-2 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44" style={{ minWidth: '11rem' }}>
                                     <div className="px-4 py-3 text-sm text-gray-900">
                                         <div className="font-medium">Pro User</div>
                                     </div>
